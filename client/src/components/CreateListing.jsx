59239cdc87635cc0aa32e417aa6ea425c0c9bae3
Francis Borja <fborja4400@gmail.com>
2021-12-14 20:04:50 -0500

    create item form functions

--- client/src/components/CreateListing.jsx
+++ client/src/components/CreateListing.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import {
   Dialog,
@@ -16,8 +17,46 @@ import {
   MenuItem,
 } from "@mui/material";
 
-const CreateListing = ({formOpen, handleFormClose}) => {
-  const [categories, setCategories] = React.useState([]);
+const CreateListing = ({ formOpen, handleFormClose }) => {
+  const [categories, setCategories] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+  });
+  const [formError, setFormError] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const addItem = async (e) => {
+    e.preventDefault();
+    console.log("good1");
+    if (document.getElementById("name").value.trim().length === 0) {
+      setFormError("Name field cannot be empty");
+    } else {
+      console.log("good2");
+      setFormError(false);
+      try {
+        const submitData = {
+          name: formData.name.trim(),
+          description: formData.description.trim(),
+          categories: categories,
+          sellerId: "61b7af9394292552b857d829",
+        }
+        console.log(submitData);
+        // TODO: change to current user's ID
+        let { data } = await axios.post(`http://localhost:4000/items`, submitData);
+        console.log(data);
+        document.getElementById("name").value = "";
+        document.getElementById("description").value = "";
+        setCategories([]);
+      } catch (error) {
+        console.log(error);
+        setFormError(error);
+      }
+    }
+  };
 
   // https://mui.com/components/selects/
   const handleCategoryChange = (event) => {
@@ -49,29 +88,33 @@ const CreateListing = ({formOpen, handleFormClose}) => {
         <TextField
           autoFocus
           margin="dense"
-          id="item-name"
+          id="name"
+          name="name"
           label="Item Name"
           type="text"
           fullWidth
           placeholder="Enter a name for your item"
           variant="standard"
+          onChange={(e) => handleChange(e)}
         />
         <TextField
           margin="dense"
-          id="item-description"
+          id="description"
           label="Item Description"
+          name="description"
           type="text"
           fullWidth
           multiline
           rows={6}
           placeholder="Describe your item"
           variant="standard"
+          onChange={(e) => handleChange(e)}
         />
         <FormControl sx={{ mt: 1, mb: 1, width: "100%" }}>
-          <InputLabel id="item-categories">Categories</InputLabel>
+          <InputLabel id="categories">Categories</InputLabel>
           <Select
-            labelId="item-categories"
-            id="item-categories"
+            labelId="categories"
+            id="categories"
             multiple
             value={categories}
             onChange={handleCategoryChange}
@@ -93,13 +136,21 @@ const CreateListing = ({formOpen, handleFormClose}) => {
           </Select>
         </FormControl>
         <FormControl sx={{ mt: 1, width: "35ch" }}>
-          <label htmlFor="item-image">Upload Image</label>
-          <Input accept="image/*" id="item-image" multiple type="file" />
+          <label htmlFor="image">Upload Image</label>
+          <Input accept="image/*" id="image" multiple type="file" />
         </FormControl>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleFormClose}>Cancel</Button>
-        <Button onClick={handleFormClose}>Submit Listing</Button>
+        <Button
+          onClick={(e) => {
+            handleFormClose();
+            addItem(e);
+          }}
+          type="submit"
+        >
+          Submit Listing
+        </Button>
       </DialogActions>
     </Dialog>
   );
